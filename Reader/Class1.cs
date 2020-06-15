6655c5beb278d211746880d1cb7a0c44ad758408
Andrew <andrew.brown314@gmail.com>
2020-06-15 01:19:27 +0100

    Added an IReader interface

--- Reader/Class1.cs
+++ Reader/Class1.cs
@@ -1,8 +1,16 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using DataDomain;
 
 namespace Reader
 {
     public class Class1
     {
+    }
+
+    public interface IReader
+    {
+        IEnumerable<ISubtitleItem> ReadFile(string fileDirectory);
     }
 }
